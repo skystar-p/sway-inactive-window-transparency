6e609d493ceec5484f1e008e84b090bcde090669
Jaehyeon Park <skystar@skystar.dev>
2022-07-20 17:07:46 +0900

    example subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use tokio_i3ipc::{event::Subscribe, I3};
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let mut conn = I3::connect().await?;
+
+    conn.subscribe([Subscribe::Window]).await?;
+
+    let mut listener = conn.listen();
+
+    while let Some(event) = listener.next().await {
+        println!("{:?}", event);
+    }
+
+    Ok(())
 }
