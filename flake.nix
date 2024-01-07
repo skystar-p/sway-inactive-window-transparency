70ae5592d1cf7deaf7822eaa05f879015fea9d5e
Jaehyeon Park <skystar@skystar.dev>
2024-01-08 02:08:32 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
-  description = "sway-inactive-window-transparency";
+  description = "sway-inactive-window-transparency flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
+  outputs = { nixpkgs, flake-utils, ... }:
+    let systems = [ "x86_64-linux" "aarch64-linux" ];
     in
-    {
-      packages = forAllSystems (system: {
-        sway-inactive-window-transparency = pkgsFor.${system}.callPackage ./. { };
-      });
-    };
+    flake-utils.lib.eachSystem systems (
+      system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        defaultPackage = (import ./. { inherit pkgs; });
+      }
+    );
 }
 
